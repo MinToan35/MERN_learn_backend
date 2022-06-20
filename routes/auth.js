a800a3c0aed646b37847c5cc2b5681dbe75cfcd4
MinToan35 <mintoan351998@gmail.com>
2022-06-20 18:17:19 +0700

    'second'

--- routes/auth.js
+++ routes/auth.js
@@ -20,7 +20,7 @@ router.post("/register", async (req, res) => {
     const hashedPassword = await argon2.hash(password);
     const newUser = new User({ username, password: hashedPassword });
     await newUser.save();
-
+    //all good
     //return token
     const accessToken = jwt.sign(
       { userId: newUser._id },
@@ -31,6 +31,47 @@ router.post("/register", async (req, res) => {
       message: "User created successfully",
       accessToken,
     });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password)
+    return res
+      .status(400)
+      .json({ success: false, message: "Missing username and/or password" });
+
+  try {
+    //check for existing user
+    const user = await User.findOne({ username });
+    if (!user)
+      return res
+        .status(400)
+        .json({ success: false, message: "Incorrect username " });
+
+    //username found
+    const passwordValid = await argon2.verify(user.password, password);
+    if (!passwordValid)
+      return res
+        .status(400)
+        .json({ success: false, message: "Incorrect password " });
+    //all good
+    //return token
+    const accessToken = jwt.sign(
+      { userId: user._id },
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    res.json({
+      success: true,
+      message: "Loggin successfully",
+      accessToken,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
 });
 module.exports = router;
